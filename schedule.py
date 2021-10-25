b80b8e98cbc36ae374148e9b3f69e35d9238d48e
Eddie <EddieChang092688@gmail.com>
2021-10-25 17:24:16 +0800

    schedule test

--- schedule.py
+++ schedule.py
@@ -5,7 +5,7 @@ from app import line_bot_api
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=17, second=30)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=18)
 def timed_job():
     line_bot_api.push_message('U4ee7f6b303c39a750a7638d340149b66', TextMessage(text='測試成功!'))
 
