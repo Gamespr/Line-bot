a9e52e0807ac697c4645be9d6d6e37af9ef2b633
Eddie <EddieChang092688@gmail.com>
2021-12-28 18:44:25 +0800

    deletion

--- schedule.py
+++ schedule.py
@@ -9,14 +9,14 @@ import requests
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=18, minute=43)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=18, minute=45)
 def timed_job():
     date = date_alarm()
     adv_date = date_adv()
     d = str(datetime.date.today())
     d1 = str(datetime.date.today() + datetime.timedelta(days=2))
     if len(adv_date) != 0:
-        line_bot_api.push_message('U4ee7f6b303c39a750a7638d340149b66',TextMessage(text='您所存放的\n' + date + '\n再過2日於' + d1 + '到期!'))
+        line_bot_api.push_message('U4ee7f6b303c39a750a7638d340149b66',TextMessage(text='您所存放的\n' + adv_date + '\n再過2日於' + d1 + '到期!'))
     if len(date) !=0:
         line_bot_api.push_message('U4ee7f6b303c39a750a7638d340149b66', TextMessage(text='您所存放的\n' + date + '\n已於今日' + d + '到期!\n請處理完食品後，於選單中點選處理食品，輸入已處理完的食品資訊'))
 
