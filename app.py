53e1703a71e2141280fa44d0f635a1a1c5a48cc9
Eddie <EddieChang092688@gmail.com>
2022-01-07 17:46:44 +0800

    arduino test

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def home():
 def test():
     # v = request.values['input_value']
     # v1 = request.values['input_value1']
-    line_bot_api.push_message('U443cbb5edf2e94e8ba23a767cf41f260',TextMessage(text='偵測到食品有腐壞的跡象，請處理腐壞的相關食品!\n'))
+    line_bot_api.push_message('U443cbb5edf2e94e8ba23a767cf41f260',TextMessage(text='偵測到食品有腐壞的跡象，請處理腐壞的相關食品!'))
     # return 'values={},{}'.format(v, v1)
     return 'success'
 
